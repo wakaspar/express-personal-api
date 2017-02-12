6c3a21eddc61332b863e08d8c7a0c33c278fb57e
Will Kaspar <Will@Aang.local>
2017-02-12 14:34:22 -0800

    on ready, list of parks in database is printed to homepage.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,28 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-// your code
+  // your code
+  var render = function(data){
+    for(var i=0; i<data.length; i++){
+      $('#results').append(`<p>${data[i].name}</p>`);
+    }
+  }
 
-});
+  var onSuccess = function(res){
+    console.log('good job, ajax');
+    console.log(res);
+    render(res);
+  }
+  var onError = function(){
+    console.log('try again, ajax');
+  }
+
+  $.ajax({
+    method: "GET",
+    url: "/api/parks/",
+    success: onSuccess,
+    error: onError
+  });
+
+
+}); // closes .ready
