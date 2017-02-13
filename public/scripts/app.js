0b05d0f13fe77be201f26eb3a7ddafbd437798bd
Will Kaspar <Will@Aang.local>
2017-02-12 19:14:27 -0800

    updated front end to allow creation of new parks - still debugging

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,37 +2,52 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-  // renders all parks in browser
-  var renderAll = function(data){
+  // renders data to the page
+  var render = function(data){
     for(var i=0; i<data.length; i++){
       if(data[i].hasVisited == false){
-      $('#placesNotYetCamped').append(
-        // html to be appended
-        `<div class="container-fluid panel">
-              <li>${data[i].name}</li>
-              <li>${data[i].location}</li>
-        </div>`);
-      } else {
-        $('#placesCamped').append(
+        $('#placesNotYetCamped').append(
           // html to be appended
           `<div class="container-fluid panel">
-                <li>${data[i].name}</li>
-                <li>${data[i].location}</li>
-                <li>${data[i].dateVisited}</li>
+          <li>${data[i].name}</li>
+          <li>${data[i].location}</li>
           </div>`);
+        } else {
+          $('#placesCamped').append(
+            // html to be appended
+            `<div class="container-fluid panel">
+            <li>${data[i].name}</li>
+            <li>${data[i].location}</li>
+            <li>${data[i].dateVisited}</li>
+            </div>`);
+          }
+        }
       }
-    }
-  }
 
+  // runs on successful Ajax call
   var onSuccess = function(res){
     console.log('good job, ajax');
-    console.log(res);
-    renderAll(res);
+    render(res);
   }
+  // runs on erroneous Ajax call
   var onError = function(){
     console.log('try again, ajax');
   }
 
+  var lilSuccess = function(res){
+    console.log('good post');
+    console.log(res);
+    if (res.option === 'off'){
+      res.hasVisited = true;
+      console.log('hasVisited ',res.hasVisited);
+    } else {
+      res.hasVisited = false;
+      console.log('hasVisited ',res.hasVisited);
+    }
+  }
+
+
+  // Ajax gets all the parks when the page loads
   $.ajax({
     method: "GET",
     url: "./api/parks/",
@@ -41,4 +56,20 @@ $(document).ready(function(){
   });
 
 
+  // 'Add new park' button event -> shows form field
+  $('#new-btn').on('click', function(){
+    $('#new-btn').removeClass('btn-outline-danger').addClass('hide-me');
+    $('#new-box').removeClass('hide-me');
+  });
+
+  // 'Form' event -> adds new park to database
+  $('#submit-btn').on('click', function(){
+    $.ajax({
+      method: "POST",
+      url: "./api/parks/",
+      data: $('form').serialize(),
+      success: lilSuccess,
+      error: onError
+    });
+  });
 }); // closes .ready
